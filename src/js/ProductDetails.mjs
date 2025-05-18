fba59cd56e7cb8a4a77975ed1c5f8a90c4dacb6d
Brigham Dean <Briggs-dean@outlook.com>
2025-05-17 17:47:01 -0800

    tst

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,61 +1,63 @@
-//Feeds into product.js which feeds the product_pages/index.html
-//contain code to dynamically produce the product details
-//also contains the addToCart method
-import {setLocalStorage, renderCartCount} from './utils.mjs';
+import { setLocalStorage, getLocalStorage, updateCartItemCount, alertMessage, clearHTMLWithMessage, loadBreadcrumbs } from './utils.mjs';
+import { findProductById } from './externalServices.mjs';
 
+let product = {};
 
-//template literal to populate the detail information for the given product
-function productDetailsTemplate(product) {
-    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-      <h2 class="divider">${product.NameWithoutBrand}</h2>
-      <img
-        class="divider"
-        src="${product.Images.PrimaryLarge}"
-        alt="${product.NameWithoutBrand}"
-      />
-      <p class="product-card__price">$${product.FinalPrice}</p>
-      <p class="product__color">${product.Colors[0].ColorName}</p>
-      <p class="product__description">
-      ${product.DescriptionHtmlSimple}
-      </p>
-      <div class="product-detail__add">
-        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-      </div></section>`;
+export default async function productDetails(productId) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  try {
+    product = await findProductById(productId);
+    // once we have the product details we can render out the HTML
+    // check to make sure the product actually exists
+    if (product) {
+      console.log(product);
+      renderProductDetails();
+      loadBreadcrumbs(getLocalStorage('so-bread'), product.NameWithoutBrand);
+
+      // once the HTML is rendered we can add a listener to Add to Cart button
+      document.getElementById("addToCart").addEventListener("click", addToCart);
+
+    } else {
+      // if the product does not exist do this
+      clearHTMLWithMessage('.product-detail', `<h3>Uh oh! The selected item doesn't exist.</h3>`)
+    }
+  } catch (e) {
+    console.log(e);
+    // if the product does not exist do this
+    clearHTMLWithMessage('.product-detail', `<h3>Uh oh! The selected item doesn't exist.</h3>`);
+  }
 }
 
-//ProductDetail class.  Data is dynamically pulled from json file based on id
-//dataSource input determine the path to the json file.  So this decides what category (tent vs backpack, etc)
-//initialization actually grabs the data for the id, then calls the renderProductDetails and contains the event listener for the addToCart
-export default class ProductDetail {
-    constructor(productId, dataSource){
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
-    }
-    async init() {
-        // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-        this.product = await this.dataSource.findProductById(this.productId);
-        // once we have the product details we can render out the HTML
-        this.renderProductDetails("main");
-        // once the HTML is rendered we can add a listener to Add to Cart button
-        // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-        document.getElementById('addToCart')
-          .addEventListener('click', this.addToCart.bind(this));
-    }
-    //simply adds the product info to the local storage.
-    addToCart(){
-            setLocalStorage('so-cart', this.product);
-            //added here to update cart counter each time you add an item
-            renderCartCount();
-    }
-    //populates the details on the product page using the template
-    //selector determines what element to attach the details to
-    renderProductDetails(selector){
-        //method to generate HTML to display our product
-        const element = document.querySelector(selector);
-        element.insertAdjacentHTML(
-            "afterBegin",
-            productDetailsTemplate(this.product)
-        )
-    }   
+function addToCart() {
+  setLocalStorage("so-cart", product);
+  updateCartItemCount();
+
+  // Get the cart icon element
+  const cartIcon = document.querySelector(".cartBackpack");
+
+  alertMessage("added to cart!")
+  // Add a CSS class to trigger the animation
+  cartIcon.classList.add("cartAnimation");
+
+  // Remove the animation class after a certain amount of time
+  setTimeout(function () {
+    cartIcon.classList.remove("cartAnimation");
+  }, 500);
+
+}
+
+function renderProductDetails() {
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Images.PrimaryLarge;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#suggestedPrice").innerText = product.SuggestedRetailPrice;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#discount").innerText = 100 - Math.round(product.FinalPrice / product.SuggestedRetailPrice * 100);
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
 }
