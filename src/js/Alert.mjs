14cd9024399513483fe00596b54035f636e36133
Brigham Dean <Briggs-dean@outlook.com>
2025-05-21 16:25:07 -0800

    alert-prog-1

---
+++ src/js/Alert.mjs
@@ -0,0 +1,11 @@
+export default class Alert {
+    constructor() {
+        this.loadAlerts();
+    }
+
+    async loadAlerts() {
+        try {
+            const response = await fetch
+        }
+    }
+}
