8d73858b93256d74ebedbb56f94ac2b6f9d3d26e
Brigham Dean <Briggs-dean@outlook.com>
2025-05-27 16:05:04 -0800

    external.mjs

---
+++ src/js/externalServices.mjs
@@ -0,0 +1,51 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else {
+    throw { name: "servicesError", message: data };
+  }
+}
+
+export async function getProductsByCategory(category) {
+  const response = await fetch(baseURL + `products/search/${category}`);
+  const data = await convertToJson(response);
+  return data.Result;
+}
+
+export async function findProductById(id) {
+  const response = await fetch(baseURL + `product/${id}`);
+  const product = await convertToJson(response);
+  return product.Result;
+}
+
+export async function checkout(payload) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  return await fetch(baseURL + "checkout/", options).then(convertToJson);
+}
+
+export async function loginRequest(creds) {
+  const options = { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(creds) };
+  const response = await fetch(baseURL + `login`, options).then(convertToJson);
+  return response.accessToken;
+}
+
+export async function getOrders(token) {
+  const options = {
+    method: "GET",
+    // the server will reject our request if we don't include the Authorization header with a valid token!
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await fetch(baseURL + "orders", options).then(convertToJson);
+  return response;
+}
