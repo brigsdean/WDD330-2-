02d4e69e186521662aaf261407563e21921cc04c
Brigham Dean <Briggs-dean@outlook.com>
2025-05-29 16:19:30 -0800

    product list sort

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,24 +1,23 @@
-//re-added product-listing page to force netlify to fix its brain
-//js for the product-listing html page
 import ExternalServices from './ExternalServices.mjs';
 import ProductList from './ProductList.mjs';
-import { loadHeaderFooter, getParams, capitalizeWord } from './utils.mjs';
+import { getParam, loadHeaderFooter } from './utils.mjs';
 
-//load header/footer wk 3
 loadHeaderFooter();
 
-const category = getParams('category');
-// first create an instance of our ProductData class.
-
+const category = getParam("category");
 const dataSource = new ExternalServices();
-// then get the element we want the product list to render in
-const listElement = document.querySelector('.product-list');
-// then create an instance of our ProductList class and send it the correct information.
-const myList = new ProductList(category, dataSource, listElement);
 
-// finally call the init method to show our products
+const title = document.querySelector("#category-title");
+
+const sortFilter = document.querySelector("#sortFilter");
+
+title.textContent = category.charAt(0).toUpperCase() + category.slice(1);
+
+
+const listElement = document.querySelector('.product-list'); // or your specific selector
+const myList = new ProductList(category, dataSource, listElement);
 myList.init();
 
-//add category name to Top Products on html page & breadcrumb
-const categoryElement = document.getElementById('category-name');
-categoryElement.innerHTML = capitalizeWord(category);
+sortFilter.addEventListener("change", () => {
+    myList.init(sortFilter.value);
+});
